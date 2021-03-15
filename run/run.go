e8be7a57d11a21550ec4f79d2f43c4f144e12c36
chen tao <hilerchyn@gmail.com>
2021-03-15 23:41:24 +0800

    实现循环调用

--- run/run.go
+++ run/run.go
@@ -18,33 +18,39 @@ func exe(cmd []string, wait int) {
 func Run(cmd []string) {
 	originalCmd := cmd
 
-	// 点亮屏幕
-	screenOnCmd := append(originalCmd, "input", "keyevent", "26")
-	log.Println("screen on")
-	exe(screenOnCmd, 5)
+	for {
+		select {
+		case <-time.Tick(630 * time.Second):
+			// 点亮屏幕
+			screenOnCmd := append(originalCmd, "input", "keyevent", "26")
+			log.Println("screen on")
+			exe(screenOnCmd, 5)
 
-	cmd = append(cmd, "am", "start", *flags.App)
-	log.Println("start app: ", cmd)
-	exe(cmd, 5)
+			startCmd := append(originalCmd, "am", "start", *flags.App)
+			log.Println("start app: ", startCmd)
+			exe(cmd, 5)
 
-	clickTabBarCmd := append(originalCmd, "input", "tap", "584", "1848")
-	log.Println("click tab bar")
-	exe(clickTabBarCmd, 12)
+			clickTabBarCmd := append(originalCmd, "input", "tap", "584", "1848")
+			log.Println("click tab bar")
+			exe(clickTabBarCmd, 12)
 
-	clickBoxCmd := append(originalCmd, "input", "tap", "995", "1665")
-	log.Println("click box")
-	exe(clickBoxCmd, 6)
+			clickBoxCmd := append(originalCmd, "input", "tap", "995", "1665")
+			log.Println("click box")
+			exe(clickBoxCmd, 6)
 
-	clickVideoCmd := append(originalCmd, "input", "tap", "555", "1320")
-	log.Println("click video button")
-	exe(clickVideoCmd, 25)
+			clickVideoCmd := append(originalCmd, "input", "tap", "555", "1320")
+			log.Println("click video button")
+			exe(clickVideoCmd, 25)
 
-	clickCloseCmd := append(originalCmd, "input", "tap", "1040", "105")
-	log.Println("click close button")
-	exe(clickCloseCmd, 10)
+			clickCloseCmd := append(originalCmd, "input", "tap", "1040", "105")
+			log.Println("click close button")
+			exe(clickCloseCmd, 10)
 
-	closeAppCmd := append(originalCmd, "am", "force-stop", "com.ss.android.article.lite")
-	log.Println("close app")
-	exe(closeAppCmd, 10)
+			closeAppCmd := append(originalCmd, "am", "force-stop", "com.ss.android.article.lite")
+			log.Println("close app")
+			exe(closeAppCmd, 10)
+
+		}
+	}
 
 }
